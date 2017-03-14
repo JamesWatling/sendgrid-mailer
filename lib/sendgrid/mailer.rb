290baa1d8ea8ee957e07d1c6aaab7eacc42b44dd
KY <infcurious@gmail.com>
2017-03-15 05:52:09 +0800

    Add support for sending mail via SMTP
    This is useful for users who use mailcatcher to test and visualize email
    locally.
    
    This change introduces 2 additional parameters in
    `Sendgrid::Mailer.initialize`
    
    1. `env`
      This is to replace `Rails.env.production?` hence decouples
    from having `Rails` as dependencies. This is not a breaking change as it
    will be default to `:production`.
    
    2. `delivery_method`
    
    There are 2 delivery methods for now, `:api` and `:smtp`. By using
    `:api`, the email will be sent via SendGrid API. By
    using `:smtp`, the email will be sent via SMTP using the `Mail` gem,
    hence the additional runtime dependency for this gem.

--- lib/sendgrid/mailer.rb
+++ lib/sendgrid/mailer.rb
@@ -1,103 +1,139 @@
-require "sendgrid/mailer/version"
-require "sendgrid/template"
+# frozen_string_literal: true
+require 'sendgrid/mailer/version'
+require 'sendgrid/template'
 require 'sendgrid-ruby'
 require 'json'
+require 'mail'
+require 'byebug'
 
 module Sendgrid
   class Mailer
     include SendGrid
 
-    def initialize(api_key, from, bcc, sandbox_mode: false, dev_catch_all: false)
+    def initialize(api_key, from, bcc, env: :production, delivery_method: :api, sandbox_mode: false, dev_catch_all: false)
       @api_key       = api_key
       @from          = from
       @bcc           = bcc
+      @env = env
+      @delivery_method = delivery_method
       @sandbox_mode  = sandbox_mode
       @dev_catch_all = dev_catch_all
     end
 
     def build_mail_json(template_id:, to: nil, from: nil, bcc: nil, substitutions: {}, options: {})
-        options = {
-          force_send: @sandbox_mode,
-        }.merge(options)
+      options = {
+        force_send: @sandbox_mode
+      }.merge(options)
 
-        if @dev_catch_all
-          to = @dev_catch_all
-          bcc = nil
-        end
-
-        SendGrid::Mail.new.tap do |m|
-          m.from = build_from(from)
-          m.template_id = template_id if template_id
-
-          m.personalizations = build_personalization(to, bcc, substitutions)
-
-          if !options[:force_send] && !Rails.env.production?
-            m.mail_settings = SendGrid::MailSettings.new.tap do |s|
-              s.sandbox_mode = SendGrid::SandBoxMode.new(enable: true)
-            end
-          end
-        end.to_json
+      if @dev_catch_all
+        to = @dev_catch_all
+        bcc = nil
       end
 
-      def send_mail(options)
+      SendGrid::Mail.new.tap do |m|
+        m.from = build_from(from)
+        m.template_id = template_id if template_id
+
+        m.personalizations = build_personalization(to, bcc, substitutions)
+
+        if !options[:force_send] && !production?
+          m.mail_settings = SendGrid::MailSettings.new.tap do |s|
+            s.sandbox_mode = SendGrid::SandBoxMode.new(enable: @sandbox_mode)
+          end
+        end
+      end.to_json
+    end
+
+    def send_mail(options)
+      case @delivery_method
+      when :smtp
+        send_mail_via_smtp(options)
+      when :api
         options = build_mail_json(options)
         send_grid.client.mail._('send').post(request_body: options)
+      else
+        raise "Unknown delivery method `#{@delivery_method}`. Available delivery methods are :smtp and :api."
+      end
+    end
+
+    private
+
+    def production?
+      @env == :production
+    end
+
+    def send_mail_via_smtp(options)
+      template_id = options[:template_id]
+      resp = send_grid.client.templates._(template_id).get
+      template = JSON.parse(resp.body)['versions'].find { |v| v['active'] == 1 }
+      body_html = template['html_content']
+      options[:substitutions].each do |k, v|
+        body_html.gsub!("{{#{k}}}", v)
       end
 
-      private
+      from = build_from(@from).email
+      ::Mail.deliver do
+        from from
+        to options[:to]
+        subject template['subject']
 
-      def build_field(field_name, object, values, defaults = nil)
-        if values && values.is_a?(Array)
-          values.each do |v|
-            object.send("#{field_name}=", parse_email(v))
-          end
-        else
-          object.send("#{field_name}=", parse_email(values || defaults))
-        end
-
-        object
-      end
-
-      def build_from(from)
-        parse_email(from || @from)
-      end
-
-      def build_personalization(to, bcc, substitutions)
-        SendGrid::Personalization.new.tap do |p|
-          p = build_field(:to, p, to)
-
-          p = build_field(:bcc, p, bcc, @bcc)
-
-          if substitutions
-            substitutions.each do |k, v|
-              p.substitutions = SendGrid::Substitution.new(
-                                  key: "{{#{k}}}",
-                                  value: v
-                                )
-            end
-          end
+        html_part do
+          content_type 'text/html; charset=UTF-8'
+          body body_html
         end
       end
+    end
 
-      def parse_email(value)
-        output = if value.is_a? Hash
-                  raise "No key :email found" unless value[:email]
-
-                  value
-                elsif split = value.match(/(.+?)<(.+)>/)
-                  {
-                    name: split[1].strip,
-                    email: split[2].strip,
-                  }
-                else
-                  { email: value.strip }
-                end
-
-        SendGrid::Email.new(output)
+    def build_field(field_name, object, values, defaults = nil)
+      if values && values.is_a?(Array)
+        values.each do |v|
+          object.send("#{field_name}=", parse_email(v))
+        end
+      else
+        object.send("#{field_name}=", parse_email(values || defaults))
       end
 
-      def send_grid
-        @sendgrid ||= SendGrid::API.new(api_key: @api_key)
+      object
+    end
+
+    def build_from(from)
+      parse_email(from || @from)
+    end
+
+    def build_personalization(to, bcc, substitutions)
+      SendGrid::Personalization.new.tap do |p|
+        p = build_field(:to, p, to)
+
+        p = build_field(:bcc, p, bcc, @bcc)
+
+        substitutions&.each do |k, v|
+          p.substitutions = SendGrid::Substitution.new(
+            key: "{{#{k}}}",
+            value: v
+          )
+        end
       end
+    end
+
+    def parse_email(value)
+      output = if value.is_a? Hash
+                 raise 'No key :email found' unless value[:email]
+
+                 value
+               elsif split = value.match(/(.+?)<(.+)>/)
+                 {
+                   name: split[1].strip,
+                   email: split[2].strip
+                 }
+               else
+                 { email: value.strip }
+              end
+
+      SendGrid::Email.new(output)
+    end
+
+    def send_grid
+      @sendgrid ||= SendGrid::API.new(api_key: @api_key)
+    end
   end
 end
---
+++ spec/sendgrid/mailer_spec.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require 'sendgrid/mailer'
+
+describe Sendgrid::Mailer do
+
+  include Mail::Matchers
+
+  it 'has a version number' do
+
+    expect(Sendgrid::Mailer::VERSION).not_to be nil
+
+  end
+
+  describe '#send_mail' do
+
+    subject { Sendgrid::Mailer.new(api_key, from, bcc, env: env, delivery_method: delivery_method, sandbox_mode: sandbox_mode, dev_catch_all: dev_catch_all) }
+
+    let(:env) { :test }
+    let(:sandbox_mode) { false }
+    let(:dev_catch_all) { false }
+    let(:template_id) { ENV['TEMPLATE_ID'] }
+    let(:api_key) { ENV['SENDGRID_API_KEY'] }
+    let(:from) { 'from@example.com' }
+    let(:to) { 'to@example.com' }
+    let(:bcc) { 'bcc@example.com' }
+    let(:options) do
+      {
+        to: to,
+        template_id: template_id,
+        substitutions: {
+          foo: 'foo',
+          bar: 'bar'
+        }
+      }
+    end
+
+    before do
+      Mail::TestMailer.deliveries.clear
+    end
+
+    context 'with smtp as delivery method' do
+
+      let(:delivery_method) { :smtp }
+
+      before do
+        subject.send_mail(options)
+      end
+
+      it { is_expected.to have_sent_email }
+      it { should have_sent_email.from(from) }
+      it { should have_sent_email.to(to) }
+
+    end
+
+    context 'with api as delivery method' do
+
+      let(:delivery_method) { :api }
+      let(:sendgrid_api) { instance_double(SendGrid::API) }
+      let(:client) { double('client') }
+      let(:mail) { double('mail') }
+      let(:send) { double('send') }
+
+      before do
+        allow(SendGrid::API).to receive(:new) { sendgrid_api }
+      end
+
+      let(:expected_requested_body) do
+        {
+          "from" => {
+            "email" => from
+          },
+          "personalizations" => [
+            {
+              "to" => [
+                {
+                  "email" => to
+                }
+              ],
+              "bcc" => [
+                {
+                  "email" => bcc
+                }
+              ],
+              "substitutions" => {
+                "{{foo}}" => "foo",
+                "{{bar}}" => "bar"
+              }
+            }
+          ],
+          "template_id" => template_id,
+          "mail_settings" => {
+            "sandbox_mode" => {
+              "enable" => sandbox_mode
+            }
+          }
+        }
+      end
+
+      it 'sends POST request to SendGrid' do
+        expect(sendgrid_api).to receive(:client) { client }
+        expect(client).to receive(:mail) { mail }
+        expect(mail).to receive(:_).with('send') { send }
+        expect(send).to receive(:post).with(request_body: expected_requested_body)
+        subject.send_mail(options)
+      end
+
+    end
+
+    context 'with unknown delivery method' do
+
+      let(:delivery_method) { :foo }
+
+      it { expect { subject.send_mail(options) }.to raise_error(RuntimeError) }
+
+    end
+
+  end
+
+end

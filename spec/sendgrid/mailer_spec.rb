5b24b4501906f9b56ce588588ed3b5f8971669a8
Justin Louie <jslouie@gmail.com>
2017-05-24 15:25:47 +0800

    Rspec physically runs... that's it

---
+++ spec/sendgrid/mailer_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+
+RSpec.describe Sendgrid::Mailer, type: :initializer do
+  let(:mailer) do
+    api_key = 'marcosface'
+    from = 'GuavaPass.com (noreply) <noreply@guavapass.com>'
+    bcc = 'dev_catchall@guavapass.com'
+    Sendgrid::Mailer.new(api_key, from, bcc)
+  end
+
+  describe '#build_mail' do
+    context '1 recipient' do
+      let(:mail_data) do
+        {
+          to: 'justin@guavapass.com',
+          template_id: '1459d62f-0c05-41a3-a5e5-875124647940',
+          substitutions: {
+            username: 'meowmixultra',
+            confirmation_url: 'https://guavapass.com',
+          },
+        }
+      end
+
+      subject { mailer.build_mail_json(mail_data) }
+
+      it { is_expected.to eq({"from"=>{"email"=>"noreply@guavapass.com", "name"=>"GuavaPass.com (noreply)"}, "personalizations"=>[{"to"=>[{"email"=>"justin@guavapass.com"}], "bcc"=>[{"email"=>"dev_catchall@guavapass.com"}], "substitutions"=>{"{{username}}"=>"meowmixultra", "{{confirmation_url}}"=>"https://guavapass.com"}}], "template_id"=>"1459d62f-0c05-41a3-a5e5-875124647940", "mail_settings"=>{"sandbox_mode"=>{"enable"=>true}}}) }
+    end
+
+    context '1 recipient with name' do
+      let(:mail_data) do
+        {
+          to: 'Justin <justin@guavapass.com>',
+          template_id: '1459d62f-0c05-41a3-a5e5-875124647940',
+          substitutions: {
+            username: 'meowmixultra',
+            confirmation_url: 'https://guavapass.com',
+          },
+        }
+      end
+
+      subject { mailer.build_mail_json(mail_data) }
+
+      it { is_expected.to eq({"from"=>{"email"=>"noreply@guavapass.com", "name"=>"GuavaPass.com (noreply)"}, "personalizations"=>[{"to"=>[{"email"=>"justin@guavapass.com", "name"=>"Justin"}], "bcc"=>[{"email"=>"dev_catchall@guavapass.com"}], "substitutions"=>{"{{username}}"=>"meowmixultra", "{{confirmation_url}}"=>"https://guavapass.com"}}], "template_id"=>"1459d62f-0c05-41a3-a5e5-875124647940", "mail_settings"=>{"sandbox_mode"=>{"enable"=>true}}}) }
+    end
+
+    context 'multi recipient with multi-format' do
+      let(:mail_data) do
+        {
+          to: ['Justin <justin@guavapass.com>', 'james@guavapass.com', { email: 'whatwhat@inthebutt.com', name: 'Kenny Southpark'}],
+          template_id: '1459d62f-0c05-41a3-a5e5-875124647940',
+          substitutions: {
+            username: 'meowmixultra',
+            confirmation_url: 'https://guavapass.com',
+          },
+        }
+      end
+
+      subject { mailer.build_mail_json(mail_data) }
+
+      it { is_expected.to eq("from"=>{"email"=>"noreply@guavapass.com", "name"=>"GuavaPass.com (noreply)"}, "personalizations"=>[{"to"=>[{"email"=>"justin@guavapass.com", "name"=>"Justin"}, {"email"=>"james@guavapass.com"}, {"email"=>"whatwhat@inthebutt.com", "name"=>"Kenny Southpark"}], "bcc"=>[{"email"=>"dev_catchall@guavapass.com"}], "substitutions"=>{"{{username}}"=>"meowmixultra", "{{confirmation_url}}"=>"https://guavapass.com"}}], "template_id"=>"1459d62f-0c05-41a3-a5e5-875124647940", "mail_settings"=>{"sandbox_mode"=>{"enable"=>true}}) }
+    end
+
+    context 'no :email, throws error' do
+      let(:mail_data) do
+        {
+          to: ['Justin <justin@guavapass.com>', 'james@guavapass.com', { emailz: 'whatwhat@inthebutt.com', name: 'Kenny Southpark'}],
+          template_id: '1459d62f-0c05-41a3-a5e5-875124647940',
+          substitutions: {
+            username: 'meowmixultra',
+            confirmation_url: 'https://guavapass.com',
+          },
+        }
+      end
+
+      subject { mailer.build_mail_json(mail_data) }
+
+      it { expect{ subject }.to raise_error RuntimeError }
+    end
+
+  end
+end
